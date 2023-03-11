943869aa27bb0f8ecd877160b7c52bcb4f576a00
Johan Grandas <dampig14@gmail.com>
2023-03-11 17:13:30 -0500

    Typescript

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,42 @@
 console.log("Hi world from typescript!");
+
+// Variables
+var name: string = "Johan";
+
+
+console.log("Hi, " + name);
+console.log("¿Qué tal", name, "?")
+console.log(`¿Como fua ${name}?`);
+
+const PI = 3.14;
+var n: any = 12;
+n = "Hola bb :O"
+
+let a:string, b:boolean, c:number;
+
+a = "Typescript"
+b = true
+c = 8.9
+
+console.log(a, b, c)
+
+
+
+let taskList: string[] = ["Task 1", "Task 2"];
+
+let anyList: any[] = ["Onions", 4]
+
+let values: (string | number | boolean)[] = ["Hi", true, 56]
+
+console.log(values.length)
+
+
+enum Running {
+    "R1" = 1,
+    "R2",
+    "R3"
+}
+
+let stateRunning: Running = Running.R2;
+
+console.log(stateRunning)
